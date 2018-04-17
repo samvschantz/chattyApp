edcdc0722a58fed39bcb22f93acb463e699827ac
Sam Schantz <samvschantz@gmail.com>
2018-04-17 02:54:55 +0000

    Setup from boilerplate to having HTML elements and CSS ready to go

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
 import React, {Component} from 'react';
 
+import Chatbar from './Chatbar.jsx';
+import MessageList from './MessageList.jsx';
+
+const MESSAGES = [{
+    type: "incomingMessage",
+    content: "I won't be impressed with technology until I can download food.",
+    username: "Anonymous1"
+},
+{
+    type: "incomingNotification",
+    content: "Anonymous1 changed their name to nomnom",
+},
+{
+    type: "incomingMessage",
+    content: "I wouldn't want to download Kraft Dinner. I'd be scared of cheese packet loss.",
+    username: "Anonymous2"
+},
+{
+    type: "incomingMessage",
+    content: "...",
+    username: "nomnom"
+},
+{
+    type: "incomingMessage",
+    content: "I'd love to download a fried egg, but I'm afraid encryption would scramble it",
+    username: "Anonymous2"
+},
+{
+    type: "incomingMessage",
+    content: "This isn't funny. You're not funny",
+    username: "nomnom"
+},
+{
+    type: "incomingNotification",
+    content: "Anonymous2 changed their name to NotFunny",
+}]
+
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <MessageList messages={MESSAGES}/>
+        <Chatbar />
+      </div>
     );
   }
 }
