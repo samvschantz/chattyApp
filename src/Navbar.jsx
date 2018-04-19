85a7f494b173727ffacfba694056082fc61ddbf5
Sam Schantz <samvschantz@gmail.com>
2018-04-19 20:07:52 +0000

    styling

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,7 +6,7 @@ class Navbar extends Component {
     return(
       <nav className="navbar">
         <a href="/" className="navbar-brand">Chatty</a>
-        <span>{this.props.numOnline} users online!</span>
+        <span id="numOnline">{this.props.numOnline} users online!</span>
       </nav>
     )
   }
